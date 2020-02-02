cfb205e53692009d050210ccf4649ce0b48e79a9
Dylan Wilson <dylanjw@protonmail.com>
2020-02-01 22:17:14 -0800

    dirty

---
+++ pacro/utils.py
@@ -0,0 +1,33 @@
+import inspect
+
+
+def _get_caller():
+    """Figure out who's calling."""
+    # Get the calling frame
+    frame = inspect.currentframe().f_back.f_back
+
+    # Pull the function name from FrameInfo
+    caller_name = inspect.getframeinfo(frame)[2]
+
+    # Get the function object
+    caller = frame.f_locals.get(
+        caller_name,
+        frame.f_globals.get(caller_name)
+    )
+
+    return caller
+
+
+def is_async_caller():
+    caller = _get_caller()
+
+    # If there's any indication that the function object is a
+    # coroutine, return True. inspect.iscoroutinefunction() should
+    # be all we need, the rest are here to illustrate.
+    if any([inspect.iscoroutinefunction(caller),
+            inspect.isgeneratorfunction(caller),
+            inspect.iscoroutine(caller), inspect.isawaitable(caller),
+            inspect.isasyncgenfunction(caller), inspect.isasyncgen(caller)]):
+        return True
+    else:
+        return False
--- test/test_bisync.py
+++ test/test_bisync.py
@@ -1,8 +1,12 @@
 import ast
+import astor
 import asyncio
 import inspect
+import time
+
 
 import pytest
+from pacro import utils
 
 """
 TODO work out "macro templates" for async/sync functions
@@ -38,61 +42,106 @@ TODO work out "macro templates" for async/sync functions
 
             '''
 
+    or write code with await keyword that can be converted to native sync code!
+        from pacro import (bisync, BisyncOption)
+        @bisync
+        async def fn():
+            await BisyncOption(
+                awaitable = asyncio.sleep(5),
+                sync_fallback = sleep(5)
+            )
+
+
 
 TODO parse "macro templates" into coroutines or regular functions by rewriting ast.
 
 """
 
+
+class BisyncOption:
+    __slots__ = ['awaitable', 'sync_fallback']
+
+    def __init__(self, awaitable, sync_fallback):
+        self.awaitable = awaitable
+        self.sync_fallback = sync_fallback
+
+def function():
+    time.sleep(5)
+
 def bisync(fn):
-    fn_src = inspect.getsource(fn)
-    def wrapper(*args, **kwargs):
-        fn_ast = ast.parse(fn_src)
-        breakpoint()
-    return wrapper
-
-
-def get_caller():
-    """Figure out who's calling."""
-    # Get the calling frame
-    frame = inspect.currentframe().f_back.f_back
-
-    # Pull the function name from FrameInfo
-    caller_name = inspect.getframeinfo(frame)[2]
-
-    # Get the function object
-    caller = frame.f_locals.get(
-        caller_name,
-        frame.f_globals.get(caller_name)
+    src = inspect.getsource(fn)
+    target_src = inspect.getsource(function)
+    print(src)
+    _tree = ast.parse(src)
+    _target_tree = ast.parse(target_src)
+    print("\n\nBEFORE PROCESSING: \n" + astor.dump_tree(_tree))
+    print("\n\nTARGET: \n" + astor.dump_tree(_target_tree))
+    tree = ast.fix_missing_locations(
+        CoroToFn().visit(_tree)
     )
+    print("\n\nAFTER PROCESSING: \n" + astor.dump_tree(tree))
+    code = compile(
+        tree,
+        filename="<bisync generated>",
+        mode="exec")
+    module = inspect.getmodule(fn)  # TODO Oh No Module Imports!!!
+    exec(code, dir(module))
+    return (namespace[fn.__name__])
 
-    return caller
+    #if utils.is_async_caller():
+    #    tree = ast.copy_location(CoroToCoro().visit(_tree))
+    #else:
+    #    tree = CoroToFn().visit(_tree)
+    #compile(
+    #    ast.fix_missing_locations(tree),
+    #    filename="<ast>",
+    #    mode="exec"
+    #)
 
 
-def is_async_caller():
-    caller = get_caller()
+class CoroToFn(ast.NodeTransformer):
+    def visit_AsyncFunctionDef(self, node):
+        node = self.generic_visit(node)
+        return ast.FunctionDef(
+            name = node.name,
+            args = node.args,
+            body = node.body,
+            decorator_list=[],  # TODO
+            returns = None,  # TODO
+            type_comment=None,  # TODO
+            )
 
-    # If there's any indication that the function object is a
-    # coroutine, return True. inspect.iscoroutinefunction() should
-    # be all we need, the rest are here to illustrate.
-    if any([inspect.iscoroutinefunction(caller),
-            inspect.isgeneratorfunction(caller),
-            inspect.iscoroutine(caller), inspect.isawaitable(caller),
-            inspect.isasyncgenfunction(caller), inspect.isasyncgen(caller)]):
-        return True
-    else:
-        return False
+    def visit_Await(self, node):
+        node = self.generic_visit(node)
+        return node.value
+
+    def visit_Call(self, node):
+        expression = [
+            keyword.value for keyword in node.keywords
+            if keyword.arg == 'sync_fallback'].pop()
+        if expression:
+            node = expression
+        return node
 
 
-@bisync
+class CoroToCoro(ast.NodeTransformer):
+    def visit_Await(self, node):
+        assert isinstance(compile(node.value, 'ast', mode="single"), BisyncOption)
+        awaitable = node.value.awaitable
+        return ast.copy_location(
+            ast.Await(awaitable),
+            node
+        )
+
+
 def is_sync_caller():
-    if is_async_caller():
+    if utils.is_async_caller():
         return False
     return True
 
 
-@bisync
 async def is_async_caller_coro():
-    if is_async_caller():
+    if utils.is_async_caller():
         await asyncio.sleep(0)
         return True
     return False
@@ -105,3 +154,19 @@ async def test_dual_sleep_coro():
 
 def test_dual_sleep_not_coro():
     assert(is_sync_caller())
+
+
+@bisync
+async def bisleep():
+    await BisyncOption(
+        awaitable = asyncio.sleep(5),
+        sync_fallback = time.sleep(5),
+    )
+
+
+def test_bisleep_sync():
+    bisleep()
+
+
+#async def test_bisleep_async():
+#    await bisleep()
